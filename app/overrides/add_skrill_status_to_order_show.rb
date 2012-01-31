37e6159aa71ec053499e2c7aa9cf799a3b3bdb39
cmar <chris@spreecommerce.com>
2012-01-31 12:59:47 -0500

    removed waiting message from order page since we refactored the checkout flow

--- app/overrides/add_skrill_status_to_order_show.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/orders/show',
-                     :name => 'add_skrill_status_to_order_show',
-                     :insert_before => "code[erb-loud]:contains('shared/order_details')",
-                     :partial => 'spree/shared/skrill_status')
